a3e7792474a7436f52592483da24b97eaf8a45eb
Liedson <liedsonaugusto@gmail.com>
2022-11-20 20:07:10 -0300

    Query findById do Seller

---
+++ src/model/dao/impc/SellerDaoJDBC.java
@@ -0,0 +1,81 @@
+package model.dao.impc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+
+import db.DB;
+import db.DbException;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+	
+	private Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if(rs.next()) {
+				int departmentID = rs.getInt("DepartmentId");
+				String departmentName = rs.getString("DepName");
+				Department dep = new Department(id, departmentName);
+				int sellerId = rs.getInt("Id");
+				String sellerName = rs.getString("Name");
+				String sellerEmail = rs.getString("Email");
+				Date sellerBirthDate = rs.getDate("BirthDate");
+				Double sellerBaseSalary = rs.getDouble("BaseSalary");
+				Seller sel = new Seller(id, sellerName, sellerEmail, sellerBirthDate, sellerBaseSalary, dep);
+				return sel;
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -17,7 +17,6 @@ public class Seller implements Serializable{
 	private Department department;
 
 	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -94,6 +93,6 @@ public class Seller implements Serializable{
 	@Override
 	public String toString() {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
-				+ baseSalary + "]";
+				+ baseSalary + ", department=" + department + "]";
 	}
 }
