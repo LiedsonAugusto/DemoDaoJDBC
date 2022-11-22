69b6ac92611b9a0509368e2763be4f90049fd6d3
Liedson <liedsonaugusto@gmail.com>
2022-11-21 21:58:40 -0300

    update e delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,10 +33,15 @@ public class Program {
 		
 		System.out.println("\nINSERT IN SELLER");
 		Seller sel = new Seller(null, "Lied", "lied@gmail.com", new Date(), 5000.0, department);
-		sellerDao.insert(sel);
-		for (Seller obj : list2) {
-			System.out.println(obj);
-		}
+		//sellerDao.insert(sel);
+		System.out.println(sellerDao.findById(8));
+		
+		System.out.println("\nUPDATE IN SELLER");
+		seller = sellerDao.findById(1);
+		seller.setName("JP");
+		sellerDao.update(seller);
+		System.out.println(seller);
+		
 		
 	}
 
