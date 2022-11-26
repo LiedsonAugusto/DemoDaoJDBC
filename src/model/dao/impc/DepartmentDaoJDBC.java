10d5607539540c8baa07adf4fbe7c7ae988d4754
Liedson <liedsonaugusto@gmail.com>
2022-11-26 10:49:19 -0300

    Começo do departmentDaoJDBC

--- src/model/dao/impc/DepartmentDaoJDBC.java
+++ src/model/dao/impc/DepartmentDaoJDBC.java
@@ -1,27 +1,89 @@
 package model.dao.impc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
 
+	private Connection conn;
+	
+	public DepartmentDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+
 	@Override
 	public void insert(Department obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO department "
+					+ "(Name) "
+					+ "VALUES "
+					+ "(?)");
+			st.setString(1, obj.getName());
+			int rowsAffected = st.executeUpdate();
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1); // pegando primeira coluna do item inserido
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			}
+		}catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
 	@Override
 	public void update(Department obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE department "
+					+ "SET Name = ? "
+					+ "WHERE "
+					+ "Id = ?");
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+			int rowsAffected = st.executeUpdate();
+			if (rowsAffected < 0) {
+				throw new DbException("Não foi possível dar update");
+			}
+		}catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"DELETE FROM department "
+					+ "WHERE "
+					+ "Id = ?");
+			st.setInt(1, id);
+			st.executeUpdate();
+		}catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
